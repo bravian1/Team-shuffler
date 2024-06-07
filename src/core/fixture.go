07959e0e7117db1b565782c39253189a69f9b6fb
bravian1 <nyatorobravian@gmail.com>
2024-06-07 19:44:23 +0300

    feat: updated fixture algorithm to include dates for the expected match

--- src/core/fixture.go
+++ src/core/fixture.go
@@ -2,18 +2,29 @@ package core
 
 import (
 	"bravian1/team-shuffler/src/types"
+	"fmt"
+	"time"
 )
 
-func Fixture(teams []types.Teams) []string {
+func Fixture(teams []types.Teams, startDate string, intervalDays int) []string {
 	n := len(teams)
 	if n%2 != 0 {
 		teams = append(teams, types.Teams{Name: "", Striker: "", Defender: ""})
 		n += 1
 	}
 
+	
+	start, err := time.Parse("2006-01-02", startDate)
+	if err != nil {
+		fmt.Println("Error parsing start date:", err)
+		return []string{}
+	}
+
 	fixtures := []string{}
 	for round := 1; round < n; round++ {
-		round_match := []string{}
+		round_date := start.AddDate(0, 0, (round-1)*intervalDays).Format("2006-01-02")
+		round_match := "Week " + fmt.Sprint(round) + " [" + round_date + ": "
+		match_list := []string{}
 		for match := 0; match <= n/2-1; match++ {
 			home := (round + match) % (n - 1)
 			away := (n - 1 - match + round) % (n - 1)
@@ -21,10 +32,14 @@ func Fixture(teams []types.Teams) []string {
 				away = n - 1
 			}
 			if !(teams[away].Name == "" || teams[home].Name == "") {
-				round_match = append(round_match, teams[home].Name+":"+teams[away].Name)
+				match_list = append(match_list, teams[home].Name+" vs "+teams[away].Name)
 			}
 		}
-		fixtures = append(fixtures, round_match...)
+		round_match += fmt.Sprintf("%s", match_list)
+		round_match += "]"
+		fixtures = append(fixtures, round_match)
 	}
+
 	return fixtures
 }
+
--- src/routes/fixtures.go
+++ src/routes/fixtures.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"os"
 	"path"
@@ -11,35 +10,42 @@ import (
 	"bravian1/team-shuffler/src/types"
 )
 
+// Fixtures generates and returns the fixtures in JSON format
 func Fixtures(w http.ResponseWriter, r *http.Request) {
 	rootDir, err := os.Getwd()
 	if err != nil {
 		http.Error(w, "Error creating fixtures", http.StatusInternalServerError)
+		return
 	}
 	filename := path.Join(rootDir, "storage", "teams.txt")
 
 	stats, err := os.Stat(filename)
-	if err!= nil {
-        http.Error(w, "Error creating fixtures", http.StatusInternalServerError)
-    }
+	if err != nil {
+		http.Error(w, "Error creating fixtures", http.StatusInternalServerError)
+		return
+	}
 	if stats.Size() == 0 {
-        http.Error(w, "Error creating fixtures", http.StatusInternalServerError)
-    }
+		http.Error(w, "Error creating fixtures", http.StatusInternalServerError)
+		return
+	}
 
 	content, err := os.ReadFile(filename)
 	if err != nil {
 		http.Error(w, "Error creating fixtures", http.StatusInternalServerError)
+		return
 	}
 	teams := []types.Teams{}
 
 	err = json.Unmarshal(content, &teams)
 	if err != nil {
 		http.Error(w, "Error creating fixtures", http.StatusInternalServerError)
+		return
 	}
+	startdate := "2024-06-10"
+	intervaldays := 3
+	fixtures := core.Fixture(teams, startdate, intervaldays)
 
-	fixtures := core.Fixture(teams)
-
-	fmt.Println(fixtures)
+	//fmt.Println(fixtures)
 	w.Header().Set("content-type", "application/json")
 	json.NewEncoder(w).Encode(fixtures)
 }
